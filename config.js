cc18287111793de8c73c78614c9cbf1ccae88cd8
SrikarSasank <103879271+SrikarSasank@users.noreply.github.com>
2023-06-02 18:06:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+apiKey: "AIzaSyA2cE4qkSEX1WBzNCXO7f9E0FQv18FDqaY",
+  authDomain: "storytellingapp-e8c94.firebaseapp.com",
+  databaseURL: "https://storytellingapp-e8c94-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "storytellingapp-e8c94",
+  storageBucket: "storytellingapp-e8c94.appspot.com",
+  messagingSenderId: "910254829262",
+  appId: "1:910254829262:web:0643d6bd4a7b0ce12d6a9f"
+  };
+
